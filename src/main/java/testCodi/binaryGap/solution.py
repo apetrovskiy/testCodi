3f8d1c3788a901c19d8f8005fc756abc5a769841
Alexander <apetrovskiy@hotmail.com>
2020-11-21 11:36:46 +0300

    .

--- src/main/java/testCodi/binaryGap/solution.py
+++ src/main/java/testCodi/binaryGap/solution.py
@@ -3,4 +3,20 @@
 
 def solution(N):
     # write your code in Python 3.6
-    pass
+    binary = str(bin(N))
+    max_gap = 0
+    previous_one = binary.find('1')
+    if previous_one == -1:
+        return max_gap
+    binary = binary[previous_one + 1:]
+    while len(binary) > 0:
+        current_one = binary.find('1')
+        if current_one != -1:
+            if max_gap < current_one:
+                max_gap = current_one
+            binary = binary[current_one + 1:]
+            previous_one = current_one
+        else:
+            break
+        
+    return max_gap
