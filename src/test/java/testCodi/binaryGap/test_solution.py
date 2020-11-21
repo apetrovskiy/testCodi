857cf5576299de0d063b080f7f5a707c82cd5f36
Alexander <apetrovskiy@hotmail.com>
2020-11-21 08:23:32 +0300

    .

---
+++ src/test/java/testCodi/binaryGap/test_solution.py
@@ -0,0 +1,34 @@
+# [1..2,147,483,647].
+import unittest
+from unittest import TestCase
+# from parameterized import parameterized
+import parameterizedtes
+from parameterized import parameterized, parameterized_class
+
+
+class SolutionTest(TestCase):
+    # @parameterized.expand(
+    #     [9, 2], [529, 4], [20, 1], [1041, 5], [15, 0], [32, 0], [1, 0], [5, 1],
+    #     [2147483647, 0], [6, 0], [328, 2], [16, 0], [
+    #         1024, 0], [11, 1], [19, 2], [42, 1],
+    #     [1162, 3], [51712, 2], [561892, 3], [
+    #         66561, 9], [6291457, 20], [74901729, 4],
+    #     [805306373, 25], [1376796946, 5], [1073741825, 29], [1610612737, 28]
+    # )
+    @parameterized(
+        (9, 2), (529, 4), (20, 1), (1041, 5), (
+            15, 0), (32, 0), (1, 0), (5, 1),
+        (2147483647, 0), (6, 0), (328, 2), (16, 0), (
+            1024, 0), (11, 1), (19, 2), (42, 1),
+        (1162, 3), (51712, 2), (561892, 3), (
+            66561, 9), (6291457, 20), (74901729, 4),
+        (805306373, 25), (1376796946, 5), (
+            1073741825, 29), (1610612737, 28)
+    )
+    def test_solution_binary_gap(self, input_number: int, expected_result: int):
+        # TODO: make it really working
+        self.assertEqual(expected_result, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- src/test/java/testCodi/solution_test.py
+++
@@ -1,8 +0,0 @@
-# [1..2,147,483,647].
-from unittest import TestCase
-
-
-class SolutionTest(TestCase):
-    def test_solution_binary_gap(self):
-        # TODO: make it really working
-        self.assertEqual(1,1)
