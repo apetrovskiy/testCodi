13a20c99456ba888146e9c7f37dd024e4fa9d45b
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-11-23 03:56:51 +0300

    .

--- src/main/java/testCodi/binaryGap/solution.rb
+++ src/main/java/testCodi/binaryGap/solution.rb
@@ -3,5 +3,29 @@
 
 def solution(n)
     # write your code in Ruby 2.2
+  binary = n.to_s(2)
+  # puts "original: " + n.to_s
+  # puts "original binary: " + binary
+  max_gap = 0
+  previous_one = binary.index('1')
+  if -1 == previous_one
+    return max_gap
+  end
+  binary = binary[previous_one + 1..-1]
+  # puts "binary: " + binary
+
+  while binary.length > 0
+    # puts binary
+    current_one = binary.index('1')
+    if nil != current_one
+      if max_gap < current_one
+        max_gap = current_one
+      end
+      binary = binary[current_one + 1..-1]
+      previous_one = current_one
+    else
+      break
+    end
+  end
+  return max_gap
 end
-  
