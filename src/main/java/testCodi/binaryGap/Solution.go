cbb8b245fda29f7cb971ceed67238ccc965a175f
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-11-25 01:48:17 +0300

    binaryGap Go and JS are done

---
+++ src/main/java/testCodi/binaryGap/Solution.go
@@ -0,0 +1,40 @@
+package solution
+
+// you can also use imports, for example:
+// import "fmt"
+// import "os"
+import "strconv"
+import "strings"
+
+// you can write to stdout for debugging purposes, e.g.
+// fmt.Println("this is a debug message")
+
+func main() {
+	Solution(1041)
+}
+
+func Solution(N int) int {
+	// write your code in Go 1.4
+	temp1 := strconv.Itoa(N)
+	temp2, _ := strconv.ParseInt(temp1, 10, 64)
+	binary := strconv.FormatInt(temp2, 2)
+
+	var maxGap int = 0
+	var previousOne = strings.Index(binary, "1")
+	if -1 == previousOne {
+		return maxGap
+	}
+	binary = binary[previousOne+1:]
+	for len(binary) > 0 {
+		var currentOne int = strings.Index(binary, "1")
+		if -1 != currentOne {
+			if maxGap < currentOne {
+				maxGap = currentOne
+			}
+			binary = binary[currentOne+1:]
+		} else {
+			break
+		}
+	}
+	return maxGap
+}
--- src/main/java/testCodi/binaryGap/solution.js
+++ src/main/java/testCodi/binaryGap/solution.js
@@ -3,4 +3,23 @@
 
 function solution(N) {
     // write your code in JavaScript (Node.js 8.9.4)
+    let binary = (N >>> 0).toString(2);
+    let maxGap = 0;
+    let previousOne = binary.indexOf("1");
+    if (-1 == previousOne) {
+        return maxGap;
+    }
+    binary = binary.substring(previousOne + 1);
+    while (binary.length > 0) {
+        let currentOne = binary.indexOf("1");
+        if (-1 != currentOne) {
+            if (maxGap < currentOne) {
+                maxGap = currentOne;
+            }
+            binary = binary.substring(currentOne + 1);
+        } else {
+            break;
+        }
+    }
+    return maxGap;
 }
