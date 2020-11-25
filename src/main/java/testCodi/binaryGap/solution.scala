4c0920e35b99863f57ff4454e4e97cac30d1d959
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-11-26 01:01:00 +0300

    binaryGap Scala is done

--- src/main/java/testCodi/binaryGap/solution.scala
+++ src/main/java/testCodi/binaryGap/solution.scala
@@ -1,3 +1,5 @@
+package testCodi.binaryGap
+
 import scala.collection.JavaConverters._
 
 // you can write to stdout for debugging purposes, e.g.
@@ -6,5 +8,24 @@ import scala.collection.JavaConverters._
 object Solution {
   def solution(n: Int): Int = {
     // write your code in Scala 2.12
+    var binary = Integer.toBinaryString(n)
+    var maxGap = 0
+    var previousOne = binary.indexOf('1')
+    if (-1 == previousOne) {
+      return maxGap
+    }
+    binary = binary.substring(previousOne + 1)
+    while (binary.length > 0) {
+      var currentOne = binary.indexOf('1')
+      if (-1 != currentOne) {
+        if (maxGap < currentOne) {
+          maxGap = currentOne
+        }
+        binary = binary.substring(currentOne + 1)
+      } else {
+        return maxGap
+      }
+    }
+    maxGap
   }
 }
