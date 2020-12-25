467099833be6c868ee6da4af5d884ef6521d3541
Alexander <apetrovskiy@hotmail.com>
2020-12-26 01:30:33 +0300

    002 is finished

---
+++ src/test/java/testCodi/a02OddOccurrencesInArray/test_odd_occurrences_in_array.py
@@ -0,0 +1,13 @@
+from src.main.java.testCodi.a02OddOccurrencesInArray.solution import solution
+import pytest
+from typing import List
+
+
+@pytest.mark.parametrize(
+    "input_array,expected_result",
+    [
+        ([9, 3, 9, 3, 9, 7, 9], 7),
+    ],
+)
+def test_odd_occurencyes_in_array(input_array: List[int], expected_result: int):
+    assert expected_result == solution(input_array)
