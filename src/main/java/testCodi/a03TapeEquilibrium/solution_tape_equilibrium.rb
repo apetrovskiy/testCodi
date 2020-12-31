22bee8350d68a2654fce23a506afbb59a3ebd487
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-12-31 18:54:05 +0300

    Ruby is done

--- src/main/java/testCodi/a03TapeEquilibrium/solution_tape_equilibrium.rb
+++ src/main/java/testCodi/a03TapeEquilibrium/solution_tape_equilibrium.rb
@@ -3,4 +3,14 @@
 
 def tape_equilibrium(a)
   # write your code in Ruby 2.2
+  sum_left = a[0]
+  sum_right = a.inject(0){|sum,x| sum + x } - a[0]
+  best_result = (sum_left - sum_right).abs
+  (1..(a.length - 2)).each do |index|
+    sum_left += a[index]
+    sum_right -= a[index]
+    current_result = (sum_left - sum_right).abs
+    best_result = current_result < best_result ? current_result : best_result
+  end
+  best_result
 end
