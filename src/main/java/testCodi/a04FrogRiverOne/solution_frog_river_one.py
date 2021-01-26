c6f04d3b74f20a1159f2c1774b97847d381e2809
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2021-01-26 15:52:24 +0300

    Perm Check Python is done

--- src/main/java/testCodi/a04FrogRiverOne/solution_frog_river_one.py
+++ src/main/java/testCodi/a04FrogRiverOne/solution_frog_river_one.py
@@ -1,13 +1,6 @@
-from src.test.java.testCodi.a04FrogRiverOne.test_frog_river_one import solution
-from typing import List
-import pytest
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
 
-
-test_data = [
-    (5, [1, 3, 1, 4, 2, 3, 5, 4], 6)
-]
-
-
-@pytest.mark.parametrize("number,input_array,expected_result", test_data)
-def test_frog_river_one(number: int, input_array: List[int], expected_result: int):
-    assert expected_result == solution(number, input_array)
+def solution(X, A):
+    # write your code in Python 3.6
+    pass
---
+++ src/test/java/testCodi/a04PermCheck/test_perm_check.py
@@ -0,0 +1,14 @@
+from src.main.java.testCodi.a04PermCheck.perm_check import solution
+from typing import List
+import pytest
+
+
+test_data = [
+    ([4, 1, 3, 2], 1),
+    ([4, 1, 3], 0)
+]
+
+
+@pytest.mark.parametrize("input,expected_result", test_data)
+def test_perm_check(input: List[int], expected_result: int):
+    assert expected_result == solution(input)
