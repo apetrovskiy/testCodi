ef3561e6d341f6623276189c757e5b5af27a394c
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-02-09 23:37:05 +0300

    preparation

---
+++ src/main/java/testCodi/problems/NextVacation/next_vacation.py
@@ -0,0 +1,5 @@
+from typing import List
+
+
+def next_vacation(input_data: List[int]) -> int:
+    return 0
---
+++ src/test/java/testCodi/problems/NextVacation/test_next_vacation.py
@@ -0,0 +1,15 @@
+from src.main.java.testCodi.problems.NextVacation.next_vacation import next_vacation
+from typing import List
+import pytest
+
+
+test_data = [
+    ([7, 3, 7, 3, 1, 3, 4, 1], 5),
+    ([2, 1, 1, 3, 2, 1, 1, 3], 3),
+    ([7, 5, 2, 7, 2, 7, 4, 7], 6)
+]
+
+
+@pytest.mark.parametrize("input,expected_result", test_data)
+def test_next_vacation(input: List[int], expected_result: int):
+    assert expected_result == next_vacation(input)
