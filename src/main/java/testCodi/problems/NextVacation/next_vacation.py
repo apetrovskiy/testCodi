ec8cdcb5c6ade0f1878d32c9c126e395245a0950
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-04-16 09:31:55 +0300

    Update next_vacation.py

--- src/main/java/testCodi/problems/NextVacation/next_vacation.py
+++ src/main/java/testCodi/problems/NextVacation/next_vacation.py
@@ -4,5 +4,5 @@ from typing import List
 def next_vacation(input_data: List[int]) -> int:
     mapping = ()
     for index in range(len(input_data)):
-        mapping.append([input_data[index],(index)])
+        mapping.append([input_data[index], (index)])
     return 0
