5bc3f57d84c74f45ae5ef9b352faf10adc4fa38a
Alexander <apetrovskiy@hotmail.com>
2021-05-09 15:05:49 +0300

    Update frog_jmp.test.js

--- src/main/java/testCodi/a03FrogJmp/frog_jmp.test.js
+++ src/main/java/testCodi/a03FrogJmp/frog_jmp.test.js
@@ -1,7 +1,7 @@
 // const sum = require("./frog_jmp.js");
 // import { sum, solution_frog_jmp } from './frog_jmp.js';
-const sum = require("./frog_jmp");
-const solution_frog_jmp = require("./frog_jmp");
+const sum = require("./frog_jmp").sum;
+const solution_frog_jmp = require("./frog_jmp").solution_frog_jmp;
 
 test("test01", () => {
     expect(solution_frog_jmp(1, 2, 1)).toBe(1);
